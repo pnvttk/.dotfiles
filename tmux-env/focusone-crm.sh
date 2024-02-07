74131bffe291e6587375dc86c598d2cb1e032aff
Pannavach T. <pannavach.t@outlook.com>
2024-02-07 20:50:30 +0700

    fix: update tmux-env sh

--- tmux-env/focusone-crm.sh
+++ tmux-env/focusone-crm.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 cd /Users/twoeight/Desktop/FocusOneCRM
 tmux new-session -s "dev-tmux" -d
-tmux split-window -h
 cd /Users/twoeight/Desktop/focus-one-link
-tmux split-window -v
+tmux split-window -h
 cd /Users/twoeight/Desktop/circulo
+tmux split-window -v
 tmux -2 attach-session -d 
 
